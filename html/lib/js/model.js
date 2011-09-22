377bc95eecac3c6a035ecb140ec8bde9c789062c
Mateu Batle <mateu.batle@collabora.co.uk>
2011-09-22 18:25:15 -0500

    Separate file list in model.js instead of hard coded in html file Populate the media library from JS First version of MediaLibrary and MediaItem classes

---
+++ html/lib/js/model.js
@@ -0,0 +1,23 @@
+// list of media files
+
+var mediaFiles = [
+    // videos
+    "media/20100525_003.mp4.avi",
+    "media/20100818_006.mp4.avi",
+    "media/20100818_011.mp4.avi",
+    "media/20101011_001.mp4.avi",
+    "media/20101105_002.mp4.avi",
+    "media/20110101_002.mp4.avi",
+    "media/20110101_003.mp4.avi",
+    "media/20110101_004.mp4.avi",
+    "media/20110101_006.mp4.avi",
+    "media/20110101_007.mp4.avi",
+
+    // images
+    "media/20100513_004.jpg",
+    "media/20100516_015.jpg",
+    "media/20110202_008.jpg",
+    "media/20110202_023.jpg",
+    "media/20110216_004.jpg",
+    "media/20110227_002.jpg"
+];
--- html/lib/js/videoeditor.js
+++ html/lib/js/videoeditor.js
@@ -1,100 +1,147 @@
-/*
- * Classes
- */
-
-// Helper to get item information from a media item
-function MediaItem (object) {
-    
-    this.Type = { 
-        IMAGE : 0,
-        VIDEO : 1,
-        AUDIO : 2,
-        TIMELINE : 3
-    };
-    
-    this.updateInfo = function(obj) {
-        if (typeof(obj) != "undefined") {
-            this._object = obj;
-            this._type = Type.VIDEO;
-        }
-
-        //if (typeof(this.object) == "MediaTimeline") {
-        //} else if (typeof(this.object) == "img") {            
-        //}
-    };
-
-    this.type = function() {
-        return this._type;
-    };
-    
-    this.id = function() {
-        return "train-entry-s1.mp4";
-    };
-    
-    // return a list of property names, match one to one with infoValues
-    this.infoFields = function() {
-        // depending on the media item type, return proper information
-        return [
-            "Source",
-            "Type",
-            "Duration",
-            "Start",
-            "Inpoint",
-            "Outpoint",
-            "Priority",
-        ];
-    };
-    
-    // return a list of proerty values, match one to one with infoFields
-    this.infoValues = function() {
-        // depending on the media item type, return proper values
-        return [
-            "Library",
-            "Video",
-            "00:01:17",
-            "00:00:00",
-            "00:00:00",
-            "00:10:00",
-            "1"
-        ];
-    }
-
-    updateInfo(object);    
-
+function dumpObject(obj) {
+  var output = '';
+  for (property in obj) {
+    output += property + ': ' + obj[property] + "\n";
+  }
+  console.log(output);
 }
 
-// library of all media items
-var MediaLibrary = function() {
-        
-    this.addMediaItem = function() {
-    };
+/*
+ * Media Item class
+ */
 
-    this.getMediaItemsByType = function(type) {        
-    };
-    
-    this.getAllMediaItems() = function() {        
-    };
+function MediaItem(filename) {
+    this._filename = filename;
+    this._extension = this._filename.split(".").pop();
+    this._thumbnailSource = this.getThumbnailSource();
 
+    // detect type
+    this._type = MediaItem.Type.UNKNOWN;
+    if (this._extension == "avi") {
+        this._type |= MediaItem.Type.VIDEO;
+    } else if (this._extension == "jpg") {
+        this._type |= MediaItem.Type.IMAGE;
+    }
+
+    // create thumbnail
+    this._thumbnail = new Image();
+    var self = this;
+    this._thumbnail.mediaItem = function() { return self; }
+    this._thumbnail.setAttribute("src", this._thumbnailSource);
+    this._thumbnail.setAttribute("width", "100%");
+    this._thumbnail.setAttribute("class", "media-library-item draggable ui-widget-content");
+
+    //dumpObject(this._thumbnail.mediaItem());
+    //dumpObject(this._thumbnail.mediaItem());
+
+    $(this._thumbnail).draggable({
+        //  use a helper-clone that is append to 'body' so is not 'contained' by a pane
+        helper: function () {
+            var newhelper = $(this).clone();
+            return newhelper.appendTo('body').css('zIndex',5).show();
+        }
+        ,   cursor: 'move'
+    });
+
+    return this;
+}
+
+Image.prototype.getMediaItem = function() {
+    return this._mediaItem;
+}
+
+MediaItem.prototype.getType = function() {
+    return this._type;
+}
+
+MediaItem.prototype.getFilename = function() {
+    return this._filename;
 };
 
-var LibraryItem = function(name) {
-    // check type of name
-    this.name = name;
-    //this.type = type;
-}();
+MediaItem.prototype.getBasename = function() {
+    var path = this._filename.split("/");
+    return path.pop();
+};
+
+MediaItem.prototype.getThumbnailSource = function() {
+    var path = this._filename.split("/");
+    var leafname = path.pop();
+    path.push("thumbnail");
+    path.push(leafname);
+    return path.join("/") + (".jpg");
+};
+
+MediaItem.prototype.getThumbnail = function() {
+    return this._thumbnail;
+};
+
+MediaItem.prototype.getProperties = function() {
+    return {
+        "Source" : "Library",
+        "Type" : "Video",
+        "Duration" : "00:01:17",
+        "Start" : "00:00:00",
+        "Inpoint" : "00:00:00",
+        "Outpoint" : "00:10:00",
+        "Priority" : 1
+    };
+}
+
+MediaItem.Type = {
+    UNKNOWN : 0,
+    IMAGE : 1,
+    VIDEO : 2,
+    AUDIO : 4,
+    TIMELINE_OBJECT : 8,
+    TIMELINE : 16
+};
+
+/*
+ * Media Library class
+ */
+
+var MediaLibrary = function() {
+    this._mediaItems = [];
+};
+
+MediaLibrary.prototype.toggleByType = function(type) {
+    for (var item in this._mediaItems) {
+        console.log(this._mediaItems[item].getType());
+        if (this._mediaItems[item].getType() & type) {
+            $(this._mediaItems[item].getThumbnail()).toggle();
+        }
+    }
+};
+
+MediaLibrary.prototype.showByType = function(type) {
+    for (var item in this._mediaItems) {
+        console.log(this._mediaItems[item].getType());
+        if (this._mediaItems[item].getType() & type) {
+            $(this._mediaItems[item].getThumbnail()).show();
+        }
+    }
+};
+
+MediaLibrary.prototype.hideByType = function(type) {
+    for (var item in this._mediaItems) {
+        console.log(this._mediaItems[item].getType());
+        if (this._mediaItems[item].getType() & type) {
+            $(this._mediaItems[item].getThumbnail()).hide();
+        }
+    }
+};
+
+MediaLibrary.prototype.addMediaFiles = function(files) {
+    for (var file in files) {
+        var item = new MediaItem(files[file]);
+        this._mediaItems.push(item);
+        $(".media-library-container").append( item.getThumbnail() );
+    }
+}
 
 /*
  * Helper functions
  */
-
-
-function toMediaSrc(obj) {
-   // return URL for the real media (not the thumbnail) 
-}
-
-function toThumbnailSrc(obj) {
-   // return URL for the thumbnail (not the real media)
-}
 
 function previewMedia(obj) {
     // check if image / video / whatever
@@ -110,9 +157,9 @@ function previewMedia(obj) {
 // Timeline to use in the video editor application
 //var editTimeline = new MediaTimeline();
 
-// media items 
-var editTimelineMediaItem = new MediaItem();
-var currentMediaItem = new MediaItem();
+// media items
+var editTimelineMediaItem = null;
+var currentMediaItem = null;
 
-// media library 
-var mediaLibrary = new MediaLibrary(); 
+// media library
+var mediaLibrary = new MediaLibrary();
