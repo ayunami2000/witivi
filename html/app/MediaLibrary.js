d02bad48e7d0966777fc0fcbea563fdb02a14e14
Mateu Batle <mateu.batle@collabora.co.uk>
2011-10-28 07:13:19 -0500

    Removed unused functions in MediaLibrary

--- html/app/MediaLibrary.js
+++ html/app/MediaLibrary.js
@@ -6,30 +6,6 @@ function MediaLibrary() {
     this._mediaItems = [];
 };
 
-MediaLibrary.prototype.toggleByType = function(type) {
-    for (var item in this._mediaItems) {
-        if (this._mediaItems[item].getType() & type) {
-            $(this._mediaItems[item].getThumbnail()).toggle();
-        }
-    }
-};
-
-MediaLibrary.prototype.showByType = function(type) {
-    for (var item in this._mediaItems) {
-        if (this._mediaItems[item].getType() & type) {
-            $(this._mediaItems[item].getThumbnail()).show();
-        }
-    }
-};
-
-MediaLibrary.prototype.hideByType = function(type) {
-    for (var item in this._mediaItems) {
-        if (this._mediaItems[item].getType() & type) {
-            $(this._mediaItems[item].getThumbnail()).hide();
-        }
-    }
-};
-
 MediaLibrary.prototype.addMediaFiles = function(files) {
     for (var file in files) {
         var item = new MediaItem(files[file]);
