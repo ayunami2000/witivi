8f72ce0c88e58b1aa8a369a581be59e16baa431c
Mateu Batle <mateu.batle@collabora.co.uk>
2011-10-28 07:26:58 -0500

    Cleanup of TransitionItem class

--- html/app/TransitionItem.js
+++ html/app/TransitionItem.js
@@ -5,40 +5,15 @@
 function TransitionItem(type) {
     this._transitionType = type;
     this._type = MediaItem.Type.TRANSITION;
+
+    // fill the properties
     this._properties = new Array();
-
+    // TODO fill the transition name
     this._properties["Name"] = "Transition";
+    this._properties["Source"] = "Library";
+    this._properties["Type"] = "Transition";
 
-    // create thumbnail
-    this._thumbnail = new Image();
-    var self = this;
-    this._thumbnail.getMediaItem = function() { return self; }
-    this._thumbnail.setAttribute("src", this.getThumbnailSource());
-    this._thumbnail.setAttribute("width", "75px");
-    this._thumbnail.setAttribute("class",
-            "media-library-item draggable ui-widget-content");
-
-    $(this._thumbnail).draggable({
-        /* Use a helper-clone that is append to 'body'
-         * so is not 'contained' by a pane.
-         */
-        helper: function (x) {
-            var newhelper = $(this).clone();
-            currentDraggedMediaItem = self;
-            return newhelper.appendTo('body').css('zIndex',5).show();
-        },
-        stop : function (x) {
-            currentDraggedMediaItem = false;
-        },
-        connectToSortable : '.media-timeline-container',
-        cursor: 'move',
-        distance: 30,
-        opacity: 0.7
-    });
-
-    $(this._thumbnail).bind('click', function() {
-        // TODO transition preview, create timeline on-the-fly to show the transition with two sample images 
-    });
+    return this;
 }
 
 TransitionItem.prototype.getType = function() {
@@ -53,17 +28,46 @@ TransitionItem.prototype.getTransitionType = function() {
     return this._transitionType;
 }
 
+TransitionItem.prototype.fillProperties = function() {
+    // nothing to do, already done :)
+}
+
 TransitionItem.prototype.getThumbnailSource = function() {
     // TODO: implement
     return "images/placeholder.png";
 }
 
 TransitionItem.prototype.getThumbnail = function() {
+    if (typeof this._thumbnail == "undefined") {
+        var self = this;
+        this._thumbnail = new Image();
+        this._thumbnail.getMediaItem = function() { return self; }
+        this._thumbnail.setAttribute("src", this.getThumbnailSource());
+        this._thumbnail.setAttribute("width", "75px");
+        this._thumbnail.setAttribute("class",
+                "media-library-item draggable ui-widget-content");
+
+        $(this._thumbnail).draggable({
+            /* Use a helper-clone that is append to 'body'
+             * so is not 'contained' by a pane.
+             */
+            helper: function (x) {
+                var newhelper = $(this).clone();
+                currentDraggedMediaItem = self;
+                return newhelper.appendTo('body').css('zIndex',5).show();
+            },
+            stop : function (x) {
+                currentDraggedMediaItem = false;
+            },
+            connectToSortable : '.media-timeline-container',
+            cursor: 'move',
+            distance: 30,
+            opacity: 0.7
+        });
+
+        $(this._thumbnail).bind('click', function() {
+            // TODO transition preview, create timeline on-the-fly to show the transition with two sample images
+        });
+    }
     return this._thumbnail;
 }
-
-TransitionItem.prototype.fillProperties = function() {
-    // nothing to do, already done :)
-}
-
-var xxx = 1;
